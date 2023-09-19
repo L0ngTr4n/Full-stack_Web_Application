b97b4fa3497871d9cd4b2ff2f4ee7cabd3397509
Hai Nguyen <s3978275@rmit.edu.vn>
2023-09-19 16:29:40 +0700

    Navbar rework
    Mostly navbar rework with some minor changes to text in html file

--- script.js
+++
@@ -1,7 +0,0 @@
-// Custom JavaScript for hover effect (optional)
-// You can use this to close the dropdown on mouseout if needed
-
-// Example: Close the dropdown on mouseout
-document.getElementById("dropdown-hover").addEventListener("mouseleave", function () {
-    this.querySelector(".dropdown-menu").style.display = "none";
-});
